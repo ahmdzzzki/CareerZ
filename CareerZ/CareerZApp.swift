c525afc2306249dac4f23f950fadec56b2eb4fe6
Ahmad Zaki <ahzaki95@gmail.com>
2026-06-19 13:06:35 +0700

    Initial Commit

---
+++ CareerZ/CareerZApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareerZApp.swift
+//  CareerZ
+//
+//  Created by Ahmad Zaki on 19/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CareerZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CareerZ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CareerZ
+//
+//  Created by Ahmad Zaki on 19/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
